4e58919a091dd4d082e67c2bd1b33eca60fd0695
ghassen <Vous@exemple.com>
2019-02-18 11:15:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+     this.state = {
+      seconds: '00',   // responsible for the seconds 
+      minutes: '00'  // responsible for the minutes
+}
+  }
+
+  functionmilliseconds() {
+    
+   let heure=new Date(2334567867879990);
+   let result=heure.toLocaleTimeString();
+
+/*  */
+    console.log(result);
+
+   return result;
+ }
+//  startCountDown() {
+//   this.intervalHandle = setInterval(this.tick, 1000);
+//   let time = this.state.minutes;
+//   this.secondsRemaining = time * 60;
+//   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="class1">
+    
+  <label  className="class">{this.functionmilliseconds()}</label>
+  <br/>
+  <label className="class2"> hour minute second</label><br/>
+{/*   <label className="danger">{this.state.minutes}</label>// */}
+
+
+
+  </div>
+
+
       </div>
     );
   }
